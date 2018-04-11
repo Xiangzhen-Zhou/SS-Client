d8ca4e199bf79fac8141dfd4c7f18e9879715dc1
zxzerster <zxzerster@gmail.com>
2018-04-11 23:10:13 +0800

    Skeleton code for network proxy setter

--- NetworkProxySetter/main.m
+++ NetworkProxySetter/main.m
@@ -7,11 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SystemConfiguration/SystemConfiguration.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        AuthorizationRef authRef;
+        AuthorizationFlags flags =  kAuthorizationFlagDefaults |
+                                    kAuthorizationFlagExtendRights |
+                                    kAuthorizationFlagInteractionAllowed |
+                                    kAuthorizationFlagPreAuthorize;
+        OSStatus ret = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, flags, &authRef);
+        assert(ret == errAuthorizationSuccess);
+        SCPreferencesRef prefRef = SCPreferencesCreateWithAuthorization(kCFAllocatorDefault, CFSTR("NetworkProxySetter"), NULL, authRef);
+        
+        NSString* path = [NSString stringWithFormat:@"/%@", kSCPrefNetworkServices];
+        NSDictionary* networkServicesSets = (NSDictionary *)SCPreferencesPathGetValue(prefRef, (__bridge CFStringRef)path);
+        for (NSString* setKey in networkServicesSets) {
+            NSDictionary* networkPref = networkServicesSets[setKey];
+            NSString* keyPath = [NSString stringWithFormat: @"%@.%@", (__bridge NSString *)kSCEntNetProxies, (__bridge NSString *)kSCPropNetProxiesProxyAutoConfigURLString];
+            NSString* autoconfigUrl = [networkPref valueForKeyPath: keyPath];
+            NSLog(@"AutoconfigURL: %@", autoconfigUrl);
+        }
     }
     return 0;
 }
