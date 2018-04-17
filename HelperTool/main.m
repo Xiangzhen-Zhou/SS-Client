c972eeb176eeee782eafe44c4d332b4adf21b785
zxzerster <zxzerster@gmail.com>
2018-04-17 21:41:24 +0800

    Add Skeleton code for HelperTool

---
+++ HelperTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelperTool
+//
+//  Created by 周向真 on 2018/4/17.
+//  Copyright © 2018年 zxzerster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
